1b72dd60127d0cb248d7fcd2f246da584afbf561
Mobarak Ali <mobarak.ali@outlook.com>
2021-02-27 11:49:51 +0600

    Making some changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -14,9 +14,23 @@ function App() {
       <header className="App-header">
           <h1>First Heading</h1>
           <p>Hi, my name is {name} and I'm {age} years</p>
+          <Hi id='55' name="Mobarak Ali"></Hi>
       </header>
     </div>
   );
 }
+
+let Hi = (prop) => {
+  const style ={
+    color: 'red',
+    backgroundColor: 'yellow',
+    padding: '15px'
+  };
+  return (
+    <div style = {style}>
+      {prop.name} {prop.id}
+    </div>
+  );
+};
 
 export default App;
